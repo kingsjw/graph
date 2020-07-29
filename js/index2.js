3f45b7df12b298f04127f313182ccf9a58c44720
kingsjw <kingsjw7@gmail.com>
2020-07-29 18:50:03 +0900

    no message

--- js/index2.js
+++ js/index2.js
@@ -1,26 +1,32 @@
 (() => {
 	class Init {
 		constructor() {
-			const canvas = document.querySelector('#graph');
-			canvas.width = 1200;
-			canvas.height = 500;
-			this.ctx = canvas.getContext('2d');
-			this.ctx.lineWidth = 1;
-			this.ctx.lineCap = 'round';
-			this.ctx.globalCompositeOperation='destination-over';
-			this.cw = canvas.width;
-			this.ch = canvas.height;
+			this.graphCanvas = document.querySelector('#graph');
+			this.rangeCanvas = document.querySelector('#range');
+			this.cw = 1200;
+			this.ch = 500;
+			this.graphCanvas.width = this.cw;
+			this.graphCanvas.height = this.ch;
+			this.rangeCanvas.width = this.cw;
+			this.rangeCanvas.height = this.ch;
+			this.graphCtx = this.graphCanvas.getContext('2d');
+			this.rangeCtx = this.rangeCanvas.getContext('2d');
 			this.location = [];
-			this.priceData = [20000, 60000, 5000, 10000, 80000, 40000, 60000, 70000, 30000, 10000, 50000, 4000, 65000, 72000];
+			this.priceData = [];
+			for (let x = 0; x < 24; x += 1) {
+				const data = Math.floor(Math.random() * (1000000 - 10000)) + 10000;
+				this.priceData.push(data);
+			}
+			console.log(this.priceData);
 			this.setLocation();
-			this.grd = this.ctx.createLinearGradient(0, 0, this.cw, 0);
-			this.grd.addColorStop(0, "#B3ACFF");
-			this.grd.addColorStop(1, "#816bff");
+			// this.grd = this.graphCtx.createLinearGradient(0, 0, this.cw, 0);
+			// this.grd.addColorStop(0, "#B3ACFF");
+			// this.grd.addColorStop(1, "#816bff");
 		}
 		setLocation() {
-			let x = 0; // 그래프의 x좌표
+			let x = 0;
 			const padding = this.cw / (this.priceData.length - 1);
-			const dataOfpx1 = this.ch / Math.max(...this.priceData); // y좌표 1px 당 data값
+			const dataOfpx1 = this.ch / Math.max(...this.priceData);
 			for (let cnt = 0; cnt < this.priceData.length; cnt += 1) {
 				const yPX = 500 - (this.priceData[cnt] * dataOfpx1);
 				this.location.push(
@@ -32,9 +38,6 @@
 				x = x + padding;
 			}
 		}
-		clear() {
-			this.ctx.clearRect(0, 0, this.cw, this.ch);
-		}
 	}
 	class Polygon extends Init {
 		constructor() {
@@ -44,17 +47,19 @@
 			this.cnt = 1;
 			this.x = 0;
 			this.y = this.ch;
-
+		}
+		clear() {
+			this.graphCtx.clearRect(0, 0, this.cw, this.ch);
 		}
 		fill() {
-			this.ctx.beginPath();
-			this.ctx.fillStyle = this.grd;
-			this.ctx.moveTo(0, 0);
-			this.ctx.lineTo(this.x, 0);
-			this.ctx.lineTo(this.x, this.y);
-			this.ctx.lineTo(0, this.y);
-			this.ctx.closePath();
-			this.ctx.fill();
+			this.graphCtx.beginPath();
+			this.graphCtx.fillStyle = this.fillColor;
+			this.graphCtx.moveTo(0, 0);
+			this.graphCtx.lineTo(this.x, 0);
+			this.graphCtx.lineTo(this.x, this.y);
+			this.graphCtx.lineTo(0, this.y);
+			this.graphCtx.closePath();
+			this.graphCtx.fill();
 		}
 		animate() {
 			if (this.x < this.cw) {
@@ -63,32 +68,68 @@
 				});
 			}
 			this.clear();
-			this.ctx.save();
+			this.graphCtx.save();
 			this.display();
-			this.ctx.save();
-			this.ctx.clip();
+			this.graphCtx.clip();
 			this.fill();
-			this.ctx.restore();
-			this.x += 10;
+			this.graphCtx.restore();
+			this.x += 50;
 		}
 		display() {
-			this.ctx.strokeStyle = this.lineColor;
-			this.ctx.beginPath();
-			this.ctx.moveTo(0, this.location[0].y);
+			this.graphCtx.strokeStyle = this.lineColor;
+			this.graphCtx.beginPath();
+			this.graphCtx.moveTo(0, this.location[0].y);
 			for (let x = 1; x < this.location.length; x += 1) {
-				this.ctx.lineTo(this.location[x - 1].x, this.location[x - 1].y);
-				this.ctx.lineTo(this.location[x].x, this.location[x].y);
+				this.graphCtx.lineTo(this.location[x - 1].x, this.location[x - 1].y);
+				this.graphCtx.lineTo(this.location[x].x, this.location[x].y);
 			}
-			this.ctx.lineTo(this.cw, this.ch);
-			this.ctx.lineTo(0, this.ch);
-			this.ctx.lineTo(0, this.location[0].y);
-			this.ctx.closePath();
-			this.ctx.stroke();
+			this.graphCtx.lineTo(this.cw, this.ch);
+			this.graphCtx.lineTo(0, this.ch);
+			this.graphCtx.lineTo(0, this.location[0].y);
+			this.graphCtx.closePath();
+			this.graphCtx.stroke();
 		}
 	}
 
+	class Range extends Init{
+		constructor() {
+			super();
+			this.fillColor = 'rgba(0,0,0,0.3)';
+			this.isDrag = false;
+			this.x1 = 0;
+			this.x2 = 0;
+			this.rangeCanvas.addEventListener('mousedown', (e) => {
+				this.x1 = e.offsetX;
+				this.isDrag = true;
+			});
+			this.rangeCanvas.addEventListener('mouseup', (e) => {
+				this.x2 = e.offsetX;
+				this.isDrag = false;
+				if (this.x1 && this.x2) {
+					this.draw();
+				}
+			});
+			this.rangeCanvas.addEventListener('mousemove', (e) => {
+				if (this.isDrag) {
+					this.x2 = e.offsetX;
+					this.draw();
+				}
+			});
+		}
+		draw() {
+			this.clear();
+			this.rangeCtx.fillStyle = this.fillColor;
+			this.rangeCtx.fillRect( this.x1, 0, this.x2 - this.x1, this.ch );
+			// console.log(this.x1, 0, this.x2 - this.x1, this.ch);
+		}
+		clear() {
+			this.rangeCtx.clearRect(0, 0, this.cw, this.ch);
+		}
+	}
+	// 드래그 해서 사각형을 만들자 그 사각형 범위 안에 들어오는 것만 필터링
 	window.onload = () => {
 		let polygon = new Polygon();
 		polygon.animate();
+		let range = new Range();
 	};
 })();

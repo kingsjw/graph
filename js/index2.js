06e973523a9782e5452affba726dbb5e8e138196
kingsjw <kingsjw7@gmail.com>
2020-07-30 13:51:09 +0900

    no message

--- js/index2.js
+++ js/index2.js
@@ -1,135 +1,141 @@
 (() => {
-	class Init {
-		constructor() {
-			this.graphCanvas = document.querySelector('#graph');
-			this.rangeCanvas = document.querySelector('#range');
-			this.cw = 1200;
-			this.ch = 500;
-			this.graphCanvas.width = this.cw;
-			this.graphCanvas.height = this.ch;
-			this.rangeCanvas.width = this.cw;
-			this.rangeCanvas.height = this.ch;
-			this.graphCtx = this.graphCanvas.getContext('2d');
-			this.rangeCtx = this.rangeCanvas.getContext('2d');
-			this.location = [];
-			this.priceData = [];
-			for (let x = 0; x < 24; x += 1) {
-				const data = Math.floor(Math.random() * (1000000 - 10000)) + 10000;
-				this.priceData.push(data);
-			}
-			console.log(this.priceData);
-			this.setLocation();
-			// this.grd = this.graphCtx.createLinearGradient(0, 0, this.cw, 0);
-			// this.grd.addColorStop(0, "#B3ACFF");
-			// this.grd.addColorStop(1, "#816bff");
-		}
-		setLocation() {
-			let x = 0;
-			const padding = this.cw / (this.priceData.length - 1);
-			const dataOfpx1 = this.ch / Math.max(...this.priceData);
-			for (let cnt = 0; cnt < this.priceData.length; cnt += 1) {
-				const yPX = 500 - (this.priceData[cnt] * dataOfpx1);
-				this.location.push(
-					{
-						x: x,
-						y: yPX,
-					}
-				);
-				x = x + padding;
-			}
-		}
+	const graphCanvas = document.querySelector('#graph');
+	const rangeCanvas = document.querySelector('#range');
+	const cw = 1200;
+	const ch = 500;
+	graphCanvas.width = cw;
+	graphCanvas.height = ch;
+	rangeCanvas.width = cw;
+	rangeCanvas.height = ch;
+	const graphCtx = graphCanvas.getContext('2d');
+	const rangeCtx = rangeCanvas.getContext('2d');
+	const location = [];
+	const priceData = [];
+	const time = 24;
+	for (let x = 0; x < time; x += 1) {
+		const data = Math.floor(Math.random() * (1000000 - 10000)) + 10000;
+		priceData.push(data);
 	}
-	class Polygon extends Init {
+	let x = 0;
+	const padding = cw / (time - 1);
+	const dataOfpx1 = ch / Math.max(...priceData);
+	for (let cnt = 0; cnt < time; cnt += 1) {
+		const yPX = 500 - (priceData[cnt] * dataOfpx1);
+		location.push(
+			{
+				x,
+				y: yPX,
+			}
+		);
+		x = x + padding;
+	}
+	class Polygon {
 		constructor() {
-			super();
 			this.lineColor = 'rgba(0,0,0,0)';
 			this.fillColor = '#B3ACFF';
 			this.cnt = 1;
 			this.x = 0;
-			this.y = this.ch;
+			this.y = ch;
 		}
 		clear() {
-			this.graphCtx.clearRect(0, 0, this.cw, this.ch);
+			graphCtx.clearRect(0, 0, cw, ch);
 		}
 		fill() {
-			this.graphCtx.beginPath();
-			this.graphCtx.fillStyle = this.fillColor;
-			this.graphCtx.moveTo(0, 0);
-			this.graphCtx.lineTo(this.x, 0);
-			this.graphCtx.lineTo(this.x, this.y);
-			this.graphCtx.lineTo(0, this.y);
-			this.graphCtx.closePath();
-			this.graphCtx.fill();
+			graphCtx.beginPath();
+			graphCtx.fillStyle = this.fillColor;
+			graphCtx.moveTo(0, 0);
+			graphCtx.lineTo(this.x, 0);
+			graphCtx.lineTo(this.x, this.y);
+			graphCtx.lineTo(0, this.y);
+			graphCtx.closePath();
+			graphCtx.fill();
 		}
 		animate() {
-			if (this.x < this.cw) {
+			if (this.x < cw) {
 				requestAnimationFrame(() => {
 					this.animate();
 				});
 			}
 			this.clear();
-			this.graphCtx.save();
+			graphCtx.save();
 			this.display();
-			this.graphCtx.clip();
+			graphCtx.clip();
 			this.fill();
-			this.graphCtx.restore();
+			graphCtx.restore();
 			this.x += 50;
 		}
 		display() {
-			this.graphCtx.strokeStyle = this.lineColor;
-			this.graphCtx.beginPath();
-			this.graphCtx.moveTo(0, this.location[0].y);
-			for (let x = 1; x < this.location.length; x += 1) {
-				this.graphCtx.lineTo(this.location[x - 1].x, this.location[x - 1].y);
-				this.graphCtx.lineTo(this.location[x].x, this.location[x].y);
+			graphCtx.strokeStyle = this.lineColor;
+			graphCtx.beginPath();
+			graphCtx.moveTo(0, location[0].y);
+			for (let x = 1; x < location.length; x += 1) {
+				graphCtx.lineTo(location[x - 1].x, location[x - 1].y);
+				graphCtx.lineTo(location[x].x, location[x].y);
 			}
-			this.graphCtx.lineTo(this.cw, this.ch);
-			this.graphCtx.lineTo(0, this.ch);
-			this.graphCtx.lineTo(0, this.location[0].y);
-			this.graphCtx.closePath();
-			this.graphCtx.stroke();
+			graphCtx.lineTo(cw, ch);
+			graphCtx.lineTo(0, ch);
+			graphCtx.lineTo(0, location[0].y);
+			graphCtx.closePath();
+			graphCtx.stroke();
 		}
 	}
 
-	class Range extends Init{
+	class Range {
 		constructor() {
-			super();
+			this.lineColor = 'rgba(0,0,0,0)';
 			this.fillColor = 'rgba(0,0,0,0.3)';
 			this.isDrag = false;
 			this.x1 = 0;
 			this.x2 = 0;
-			this.rangeCanvas.addEventListener('mousedown', (e) => {
+			this.selectCnt1 = null;
+			this.selectCnt2 = null;
+			rangeCanvas.addEventListener('mousedown', (e) => {
+				this.clear();
 				this.x1 = e.offsetX;
+				this.selectCnt1 = Math.ceil(this.x1 / padding);
 				this.isDrag = true;
 			});
-			this.rangeCanvas.addEventListener('mouseup', (e) => {
+			rangeCanvas.addEventListener('mouseup', (e) => {
 				this.x2 = e.offsetX;
 				this.isDrag = false;
-				if (this.x1 && this.x2) {
-					this.draw();
-				}
+				this.selectCnt2 = Math.ceil(this.x2 / padding);
+				this.fill();
+				this,selectCnt1 = null;
+				this.selectCnt2 = null;
 			});
-			this.rangeCanvas.addEventListener('mousemove', (e) => {
+			rangeCanvas.addEventListener('mousemove', (e) => {
 				if (this.isDrag) {
 					this.x2 = e.offsetX;
-					this.draw();
+					this.selectCnt2 = Math.ceil(this.x2 / padding);
+					this.fill();
 				}
 			});
 		}
-		draw() {
+		fill() {
 			this.clear();
-			this.rangeCtx.fillStyle = this.fillColor;
-			this.rangeCtx.fillRect( this.x1, 0, this.x2 - this.x1, this.ch );
-			// console.log(this.x1, 0, this.x2 - this.x1, this.ch);
+			rangeCtx.fillStyle = this.fillColor;
+			const startX = this.selectCnt1 < this.selectCnt2 ? this.selectCnt1 - 1 : this.selectCnt1;
+			const lastX = this.selectCnt1 < this.selectCnt2 ? this.selectCnt2 : this.selectCnt2 - 1;
+			if (location[startX] && location[lastX]) {
+				rangeCtx.beginPath();
+				rangeCtx.moveTo(location[startX].x, 0);
+				rangeCtx.lineTo(location[lastX].x, 0);
+				rangeCtx.lineTo(location[lastX].x, ch);
+				rangeCtx.lineTo(location[startX].x, ch);
+				rangeCtx.lineTo(location[startX].x, 0);
+				rangeCtx.closePath();
+				rangeCtx.fill();
+			}
 		}
 		clear() {
-			this.rangeCtx.clearRect(0, 0, this.cw, this.ch);
+			rangeCtx.clearRect(0, 0, cw, ch);
 		}
 	}
 	// 드래그 해서 사각형을 만들자 그 사각형 범위 안에 들어오는 것만 필터링
 	window.onload = () => {
 		let polygon = new Polygon();
 		polygon.animate();
-		let range = new Range();
+		new Range();
+		// range.display();
 	};
 })();
